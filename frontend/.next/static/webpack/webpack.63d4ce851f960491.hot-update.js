66737f376a737b397b4f03bf29152332a25cf68c
Tejasvi Addagada <tejasvichand@gmail.com>
2025-06-19 15:45:34 +0530

    added backend file

--- frontend/.next/static/webpack/webpack.63d4ce851f960491.hot-update.js
+++ frontend/.next/static/webpack/webpack.63d4ce851f960491.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ (() => {
-/******/ 	__webpack_require__.h = () => ("1be2c0a8d6b6064b")
+/******/ 	__webpack_require__.h = () => ("6ef8781a932bd610")
 /******/ })();
 /******/ 
 /******/ }
